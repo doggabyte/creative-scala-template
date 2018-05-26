473417e6e914f3c24ad8a71bff177877233bac98
Andreas Pettersson <andreas.pettersson@csamhealth.com>
2018-05-26 18:19:59 +0200

    triangles

--- src/main/scala/Xlnt.scala
+++ src/main/scala/Xlnt.scala
@@ -15,5 +15,23 @@ package doggabyte {
       arch.draw
       house.draw
     }
+
+    def cross(count: Int): Image = {
+      val unit = Image.circle(20)
+      count match {
+        case 0 => unit
+        case n => unit beside (unit above cross(n-1) above unit) beside unit
+      }
+    }
+
+    def evilTriangle(count: Int): Image = {
+      val triangle = Image.triangle(8, 8) lineColor Color.black
+      count match {
+        case 0 => triangle above (triangle beside triangle)
+        case n =>
+          val unit = evilTriangle(n-1)
+          unit above (unit beside unit)
+      }
+    }
   }
 }
